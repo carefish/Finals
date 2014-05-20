ce094bb0503a7ccaa228843c2697e1fb87ad4814
Tim Schoonman <root@is1337.eu>
2014-05-20 14:50:08 +0200

    added sliders'n'such, to customize during playtesting.

--- Assets/Scripts/UI/PauseConditions.cs
+++ Assets/Scripts/UI/PauseConditions.cs
@@ -6,8 +6,9 @@ public class PauseConditions : MonoBehaviour
     void Update()
     {
         // Pause game if less or more than 4 fingers (2 players)
+        
 #if UNITY_ANDROID
-        if (Input.touchCount >= 4)
+        if (Input.touchCount == 4)
         {
             Time.timeScale = 1.0f;
         }
