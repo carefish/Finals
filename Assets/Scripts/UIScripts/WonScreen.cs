8a140240fe7be88be5ef2522a240d1e22862e123
Ferdi Stoeltie <ferdistoeltie@gmail.com>
2014-06-10 19:34:59 +0200

    uiUpdate

--- Assets/Scripts/UIScripts/WonScreen.cs
+++ Assets/Scripts/UIScripts/WonScreen.cs
@@ -5,9 +5,9 @@ using System.Collections;
 /// </summary>
 public class WonScreen : MonoBehaviour {
 	// Gameobjects needed for won screen:
-	private GameObject wonBG;
-	private GameObject button_LvlSelect;
-	private GameObject darkOverlay;
+	private GUITexture wonBG;
+	private GUITexture button_LvlSelect;
+	private GUITexture darkOverlay;
 	private GameObject guiWon;
 	//---------------------------
 	/// <summary>
@@ -25,26 +25,27 @@ public class WonScreen : MonoBehaviour {
 	void Start () {
 		if(!levelWon)	{
 			guiWon 		= GameObject.Find("guiWon");
-			darkOverlay = GameObject.Find("final_overlay");
-			button_LvlSelect = GameObject.Find("btn_lvlslct_up");
-			wonBG 	= GameObject.Find("ui_popup_final");
-			bWidth  *= wonBG.transform.parent.parent.GetComponent<ScaleInGameGUI>().getScaleFactorX();
-			bHeight *= wonBG.transform.parent.parent.GetComponent<ScaleInGameGUI>().getScaleFactorY();
+			darkOverlay = GameObject.Find("final_overlay").GetComponent<GUITexture>();
+			button_LvlSelect = GameObject.Find("btn_lvlslct_up").GetComponent<GUITexture>();
+			wonBG 	= GameObject.Find("ui_popup_final").GetComponent<GUITexture>();
+			ScaleInGameGUI getInfo = transform.parent.GetComponent<ScaleInGameGUI>();
+			bWidth  *= getInfo.getScaleFactorX();
+			bHeight *= getInfo.getScaleFactorY();
 
-			tWidth 	*= wonBG.transform.parent.parent.GetComponent<ScaleInGameGUI>().getScaleFactorX();
-			tHeight *= wonBG.transform.parent.parent.GetComponent<ScaleInGameGUI>().getScaleFactorY();
+			tWidth 	*= getInfo.getScaleFactorX();
+			tHeight *= getInfo.getScaleFactorY();
 			// wonBG scale:
 			wonBG.GetComponent<GUITexture>().pixelInset = new Rect(Screen.width / 2 - tWidth / 2 , Screen.height / 2 - tHeight / 2 , tWidth, tHeight);
 			button_LvlSelect.GetComponent<GUITexture>().pixelInset = new Rect(Screen.width / 2 - bWidth / 2 , Screen.height / 2.1f - bHeight , bWidth, bHeight);
 			Rect getTextWindow = guiWon.GetComponent<GUIText>().GetScreenRect();
 			Debug.Log("textwindow : " + getTextWindow.width);
-			guiWon.GetComponent<GUIText>().pixelOffset = new Vector2(Screen.width / 2 - getTextWindow.width / 2, Screen.height / 2 + 100 * wonBG.transform.parent.parent.GetComponent<ScaleInGameGUI>().getScaleFactorY());
+			guiWon.transform.GetComponent<GUIText>().pixelOffset = new Vector2(Screen.width / 2 - getTextWindow.width / 2, Screen.height / 2 + 100 * wonBG.transform.parent.parent.GetComponent<ScaleInGameGUI>().getScaleFactorY());
 			//guiWon.GetComponent<MeshRenderer>().renderer.bounds.size.Set(guiWon.GetComponent<MeshRenderer>().renderer.bounds.size.x / 2, guiWon.GetComponent<MeshRenderer>().renderer.bounds.size.y, 0);
-			guiWon.SetActive(false);
 			// Set gameobjects off
-			darkOverlay.SetActive(false);
-			wonBG.SetActive(false);
-			button_LvlSelect.SetActive(false);
+			guiWon.SetActive(false);
+			darkOverlay.enabled = false;
+			wonBG.enabled = fals
+			button_LvlSelect.enabled = false;
 			levelWon = false;
 		}
 	}
@@ -55,9 +56,10 @@ public class WonScreen : MonoBehaviour {
 		if(levelWon)	{
 			// Set gameobjects on
 			guiWon.SetActive(true);
-			darkOverlay.SetActive(true);
-			wonBG.SetActive(true);
-			button_LvlSelect.SetActive(true);
+			darkOverlay.enabled = true;
+			wonBG.enabled = true;
+			//wonBG.SetActive(true);
+			button_LvlSelect.enabled = true;
 			if(button_LvlSelect.GetComponent<GUITexture>().HitTest(Input.mousePosition) && Input.GetMouseButtonDown(0))	{
 				Application.LoadLevel(0);
 			}
