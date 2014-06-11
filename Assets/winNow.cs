cdbd9527790e16230385877c5712a1607ef8190b
Tim Schoonman <root@is1337.eu>
2014-06-11 14:44:26 +0200

    level 3 beginnings. about to merge w/ ferdi's code

--- Assets/winNow.cs
+++ Assets/winNow.cs
@@ -1,20 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class winNow : MonoBehaviour {
+public class winNow : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	void OnCollisionEnter (Collision col)	{
-		if(col.collider.name == "obj_Player(Clone)")	{
-			Debug.Log("hitting the finish line");
-		}
-	}
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.name == "obj_Player(Clone)")
+        {
+            Debug.Log("hitting the finish line");
+        }
+    }
 }
